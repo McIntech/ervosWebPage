407f4b0e93ffe5346e47da6f6cdb53cbe0f33ed0
Franco Limón <francoenriquee5@gmail.com>
2023-10-12 21:46:22 -0600

    quitar alert

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 // test para la unidad de medida VH de CSS
 // escrito por BCasal
 
-(function() {
-	if (!Modernizr.cssvhunit) {
-		alert('Este navegador «NO» soporta VH');
-	} else {
-		alert('Este navegador «SI» soporta VH');
-	}
-}).call(this);
+
